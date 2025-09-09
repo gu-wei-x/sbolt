7e4205f0a44623a741324a36f8543c006583cf74
Wei Gu <140672679+gu-wei-x@users.noreply.github.com>
2025-09-09 15:08:56 -0700

    [wiki]Add readme wiki (#3)
    * [wiki]Add readme wiki
    
    ---------
    
    Co-authored-by: Wei Gu <gu_wei_dev@outlook.com>

--- rust/core/lib/src/codegen/mod.rs
+++ rust/core/lib/src/codegen/mod.rs
@@ -1,11 +1,18 @@
 pub(crate) mod views;
 
 use std::collections::HashMap;
+use std::env;
 use std::path::PathBuf;
 
 use crate::utils;
 
-pub fn process_views(view_dir: PathBuf, temp_dir: PathBuf, crate_name: &str) -> PathBuf {
+// called by build.rs to process view templates and generate intermediate files.
+// returns the path to the generated mod.rs file which contains all view modules.
+// mod.rs path will be used in the include! macro in the main.rs file of the application.
+// TODO: returns result instead of panicking on errors.
+pub fn process_views(view_dir: &str, crate_name: &str) -> PathBuf {
+    let view_dir = PathBuf::from(view_dir);
+    let view_dir = view_dir.canonicalize().unwrap();
     if (view_dir.exists() == false) || (view_dir.is_dir() == false) {
         panic!("view folder does not exist or is not a folder.");
     }
@@ -15,6 +22,9 @@ pub fn process_views(view_dir: PathBuf, temp_dir: PathBuf, crate_name: &str) -> 
     if template_files.is_empty() {
         panic!("no view files found");
     }
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let temp_dir = PathBuf::from(out_dir).join("views");
 
     _ = std::fs::create_dir_all(temp_dir.clone());
     let mut imported_mods: Vec<String> = vec![];
@@ -42,5 +52,8 @@ pub fn process_views(view_dir: PathBuf, temp_dir: PathBuf, crate_name: &str) -> 
         &view_name_mapping,
     );
 
+    // Tell cargo to rerun the build script if the view directory changes.
+    println!("cargo:rerun-if-changed={}", view_dir.display());
+    println!("cargo:rustc-env=VIEW_FILES={}", mod_file_path.display());
     mod_file_path
 }
--- rust/examples/basic/build.rs
+++ rust/examples/basic/build.rs
@@ -1,20 +1,7 @@
 // `cargo build -vv` to show output.
 use disguise;
 use std::env;
-use std::path::PathBuf;
 
 fn main() {
-    // 1. generate intermediate files for view templates
-    // 2. generate a list file which contains all generated intermediate file paths.
-    // 3. use include! macro to include list file in app main.rs
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let view_dir = PathBuf::from("src/views");
-    let temp_dir = PathBuf::from(out_dir).join("views");
-
-    let view_dir = view_dir.canonicalize().unwrap();
-    println!("cargo:rerun-if-changed={}", view_dir.display());
-
-    let crate_name = format!("{}_views", env!("CARGO_PKG_NAME"));
-    let generated_index = disguise::process_views(view_dir, temp_dir, &crate_name);
-    println!("cargo:rustc-env=VIEW_FILES={}", generated_index.display());
+    _ = disguise::process_views("src/views", &format!("{}_views", env!("CARGO_PKG_NAME")));
 }
