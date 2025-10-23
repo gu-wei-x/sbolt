cc4c3bdb5d6f17d8d01336ccee78840756c4b448
Wei Gu <140672679+gu-wei-x@users.noreply.github.com>
2025-10-22 21:49:38 -0700

    [ref]Share views/templates between different crates (#53)
    Co-authored-by: Wei Gu <gu_wei_dev@outlook.com>

--- rust/core/lib_it/no_op/build.rs
+++ rust/core/lib_it/no_op/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     let option = sbolt::codegen::CompilerOptions::default()
-        .with_source_dir("views")
-        .with_mod_name("lib_it_views");
+        .with_source_dir("../views")
+        .with_mod_name("lib_it_no_op_views");
     let compiler = sbolt::codegen::Compiler::new(option);
     compiler.compile();
 }
